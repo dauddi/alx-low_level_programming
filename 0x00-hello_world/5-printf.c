7491df36f882f372aba4307c2b495ae6eb84bdfa
Davis Okioma <kuntaytc@gmail.com>
2021-06-17 17:31:37 +0300

    error fixes on 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: 0 //success
+ */
 int main(void)
 {
 char str[] = "with proper grammar, but the outcome is a piece of art,";
 
 printf("%s\n", str);
-return 0;
+return (0);
 }
