cec833827eccbf8047bfe537a06bad8c8534d6c4
Davis Okioma <kuntaytc@gmail.com>
2021-06-17 17:22:28 +0300

    error fixes on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: 0 //success
+ */
 int main(void)
 {
   char str[] = "\"Programming is like building a multilingual puzzle";
-  
   puts(str);
   return (0);
 }
