d67730ac02a0a66ce374f3b3ad88bfacbb38eda9
Davis Okioma <kuntaytc@gmail.com>
2021-06-19 14:28:51 +0300

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - number stored in the variable n is positive or negative.
+ *Return: 0
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'z'; chr >= 'a'; chr--)
+		putchar(chr);
+	putchar('\n');
+
+	return (0);
+}
