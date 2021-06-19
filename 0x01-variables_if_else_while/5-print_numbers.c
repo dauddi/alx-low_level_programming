57651a1dec5db29d1ca6e3f6e9e5d04befa2f001
Davis Okioma <kuntaytc@gmail.com>
2021-06-19 13:18:17 +0300

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - number stored in the variable n is positive or negative.
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+
+	return (0);
+}
