da5c20c401fcc69c8235e1f9deab1b658fede090
Davis Okioma <kuntaytc@gmail.com>
2021-06-19 13:14:18 +0300

    4-alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - number stored in the variable n is positive or negative.
+ *Return: 0
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		if (chr != 'q' && chr != 'e')
+			putchar(chr);
+	}
+	putchar('\n');
+
+	return (0);
+}
