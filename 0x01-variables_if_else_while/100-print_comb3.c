6820f0252e5027fd17b7a2685d84ecf23c20c152
Davis Okioma <kuntaytc@gmail.com>
2021-06-21 11:12:09 +0300

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,28 +7,28 @@
  *Return: 0
  */
 
-int main(void) {
-  
-  int x , y;
+int main(void)
+{
+	int x , y;
 
-  for (x = '0'; x <= '9'; x++)
-  {
-   	for (y = '0'; y <= '9'; y++)
-    {
-      if (x < y)
-      {
-        putchar(x);
-        putchar(y);
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			if (x < y)
+			{
+				putchar(x);
+			    putchar(y);
 
-        if (!(x == '8' && y == '9'))
-        {
-          putchar(',');
-          putchar(' ');
-        }
-      }
-    } 
-  }
-  putchar('\n');
+			    if (!(x == '8' && y == '9'))
+			    {
+			    	putchar(',');
+			      	putchar(' ');
+			    }
+			}
+		}
+	}
+	putchar('\n');
 
-  return (0);
+	return (0);
 }
