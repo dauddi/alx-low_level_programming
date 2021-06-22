271167c5339b33edf234e01630555f4d116b290a
Davis Okioma <kuntaytc@gmail.com>
2021-06-22 14:29:26 +0300

    2 10xalphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - prints alphabet ten times
+ *Return: void.
+ */
+
+void print_alphabet_x10(void)
+{
+	char chr;
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (chr = 'a'; chr <= 'z'; chr++)
+		{
+			_putchar(chr);
+		}
+		_putchar('\n')
+	}
+}
