301da621bc87afb6a094e7f20541be55fe0580c8
Davis Okioma <kuntaytc@gmail.com>
2021-06-22 13:45:11 +0300

    1-prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "holberton.h"
+
+/**
+ *main - number stored in the variable n is positive or negative.
+ *Return: 0
+ */
+
+int main(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		_putchar(chr);
+	}
+	_putchar('\n')
+
+	return (0);
+}
