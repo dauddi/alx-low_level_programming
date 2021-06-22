217a59f1c11183dfee4419a44e7985d98235b92f
Davis Okioma <kuntaytc@gmail.com>
2021-06-22 14:45:24 +0300

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *_islower - checks for lowercase characters
+ *Return: 1 or 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 || c <= 122)
+		return (1);
+	return (0);
+}
