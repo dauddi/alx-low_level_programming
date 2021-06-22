bb1e92beb7a70b8eb4e29b677aae61d05e9e4670
Davis Okioma <kuntaytc@gmail.com>
2021-06-22 17:01:12 +0300

    8. 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ *print_last_digit - prints the last digit of a number
+ * @n: user input to be computed
+ *
+ *Return: 1, -1 or 0.
+ */
+
+void checkDigits(char h);
+
+void jack_bauer(void)
+{
+	char h = 0;
+	char m = 0;
+
+	do
+	{
+		checkDigits(h);
+		_putchar('h');
+		_putchar(':');
+		checkDigits(m);
+		_putchar('m');
+		_putchar('\n');
+
+		if (m == 60)
+		{
+			h++;
+			m -= 59;
+		}
+	}while(h < 23 && m != 59)
+}
+
+void checkDigits(char h)
+{
+	if (!(h % 100))
+	{
+		_putchar('0');
+	}
+	return;
+}
