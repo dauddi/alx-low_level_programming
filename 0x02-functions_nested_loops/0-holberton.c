8a5b9745c87b7807d7a34a7da3d316c2b562a824
Davis Okioma <kuntaytc@gmail.com>
2021-06-22 13:29:18 +0300

    0-prints holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,14 +6,6 @@
  *Return: 0
  */
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
 int main(void)
 {
 	char str[] = "Holberton", newLine = '\n';
