58d2b6fc61044944804c34f0c95d7645777d0182
Davis Okioma <kuntaytc@gmail.com>
2021-06-22 15:03:37 +0300

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *_isalpha - checks for alphabetic characters
+ * @c: user input to be checked
+ * 
+ *Return: 1 or 0.
+ */
+
+int _isalpha(int c)
+{
+        if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+                return (1);
+        return (0);
+}
