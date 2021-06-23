db579e40eaf1e9d0dd109166575133e68cf4d71d
Davis Okioma <kuntaytc@gmail.com>
2021-06-23 12:27:28 +0300

    11. Prints to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *print_to_98 - sums two ints
+ * @n: user 
+ * 
+ *Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (;n <= 98; n++)
+			printf("%d", n);
+		if (n != 98)
+		{
+			printf(", ");
+		}
+	}
+	else
+	{
+		for (;n >= 98; n--)
+			printf("%d", n);
+		if (n != 98)
+		{
+			printf(", ");
+		}
+	}
+}
