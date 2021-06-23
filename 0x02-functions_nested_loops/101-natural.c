6e48227f9e5fb8dab9dae1288cadfaf4d93675bb
Davis Okioma <kuntaytc@gmail.com>
2021-06-23 13:17:45 +0300

    101. prints sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ *sumMultiples - computes sum of all mulpiples of 3 and 5
+ * main - main function
+ * 
+ * @n: user input
+ *
+ *Return: Always 0
+ */
+
+int sumMultiples(int n);
+
+int main(void)
+{
+	int n = 10;
+	
+	sumMultiples(n);
+}
+
+int sumMultiples(int n)
+{
+	int x;
+	int sum = 0;
+
+	for (x = 0; x < n; x++)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			sum += x;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
