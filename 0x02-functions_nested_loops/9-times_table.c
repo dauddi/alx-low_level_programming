3133a7d51c5dafe7dcb0d2355937b6ce7785b60c
Davis Okioma <kuntaytc@gmail.com>
2021-06-23 11:36:43 +0300

    9. times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *times_table - prints the 9 times table starting with 0
+ *
+ *Return: void
+*/
+
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			y *= x;
+
+			_putchar(y);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
