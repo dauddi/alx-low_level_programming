293144580a5e842ae1a249cab53c240c2d7e3880
Davis Okioma <kuntaytc@gmail.com>
2021-06-23 12:08:20 +0300

    10. sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *add - sums two ints
+ * @x: user input to be computed
+ * @y: user input to be computed 
+ * 
+ *Return: sum
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
