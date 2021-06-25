2605aa683014970b675d50a3d97a10b9158167c4
Davis Okioma <kuntaytc@gmail.com>
2021-06-25 12:51:24 +0300

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - prints numbers from 0 to 9 and a newline
+ *
+ *Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar((i + '0'));
+	}
+	_putchar('\n');
+}
