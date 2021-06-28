2215b106e1d41a4dec2c0a6521c6426b52dad6ec
Davis Okioma <kuntaytc@gmail.com>
2021-06-28 22:07:18 +0300

    5-reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ * rev_string - function that reverses a string
+ * @s: user input string
+ * _strlen - returns length of string
+ * @str: input string
+ * rev_string - prints a string to stdout
+ * @s: array of chars
+ * Return: void
+ */
+
+int _strlen(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s)
+{
+	s = print_rev(s);
+}
+
+void print_rev(char *s)
+{
+	int i, length;
+
+	char chr;
+
+	length = _strlen(str);
+
+	for (i = (length - 1); i >= 0; i++)
+	{
+		chr = str[i];
+		_putchar(chr);
+	}
+	_putchar('\n');
+}
+
+int _strlen(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
+
