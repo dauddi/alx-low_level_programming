9e9de7956324cb9e40babafbb405188b75085a62
Davis Okioma <kuntaytc@gmail.com>
2021-07-01 15:59:13 +0300

    0-strcat_concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * _strcat- Concatenates dest with src
- *@dest: input string 
+ *@dest: input string
  *@src: input string to be appended
  * Return: string
  */
@@ -13,11 +13,13 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; dest[i]; i++)
 	{
 	}
+
 	for (j = 0; src[j]; j++)
 	{
 		dest[i] = src[j];
 		i++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
