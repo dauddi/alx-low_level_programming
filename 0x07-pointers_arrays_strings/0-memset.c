4ce7aa3946479d66d38d17bd5da5fa162ef76b43
Davis Okioma <kuntaytc@gmail.com>
2021-07-05 14:39:22 +0300

    0-memset_fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 
+/**
+ * _memset - fills first bytes of memory with constant byte.
+ *@s: user input string
+ *@b: Constant byte
+ *@n: Size
+ * Return: s
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
+		*s = 'b';
+		*s++;
 	}
 	return s;
 }
