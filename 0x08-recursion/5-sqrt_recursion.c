773f46ed09ff32710636a40999dc64d20268cc48
Davis Okioma <kuntaytc@gmail.com>
2021-07-08 16:51:43 +0300

    5-sqrt_recursion.c_ returns the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns a natural squareroot of a number
+ *@n:integer
+ * Return: square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	int x, y;
+	int *px = &x;
+
+	*px = 1;
+	y = x;
+
+	if (n == 1)
+	{
+		return (1);
+	}
+	else if (y > n/2)
+	{
+		return (-1);
+	}
+	else if (n/y == y)
+	{
+		return (y);
+	}
+	else
+	{
+		*px += 1;
+		return (_sqrt_recursion(n));
+	}
+}
