9670022c0524fb18955155a5979960da919280f9
Davis Okioma <kuntaytc@gmail.com>
2021-07-08 14:58:02 +0300

    4-pow_recursion.c_ returns the value of a number raised to the power of a given number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,7 +14,7 @@ int _pow_recursion(int x, int y)
 	else if (y == 0)
 		return (1);
 	else if (y == 1)
-		return x;
+		return (x);
 	else
-		return (x * _pow_recursion(x , y--));
+		return (x * _pow_recursion(x, y--));
 }
