1ecf3e1aa32ccac3172c84062964827eda2dde28
Davis Okioma <kuntaytc@gmail.com>
2021-07-08 14:54:52 +0300

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns the factorial of a given number
+ *@x:base number
+ *@y:superscript
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return x;
+	else
+		return (x * _pow_recursion(x , y--));
+}
