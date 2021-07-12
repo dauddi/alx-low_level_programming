32d2a36489cc5631e61bf132c90a84c32e9ef21b
Davis Okioma <kuntaytc@gmail.com>
2021-07-12 10:55:26 +0300

    0-whatsmyname.c_ prints the program's name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - prints function name
+ *@argc: size of argv array
+ *@argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return 0;
+}
