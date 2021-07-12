b954e8b52e262bca9ee8c0fab9a69846d2daee0a
Davis OKioma <64232602+kunta969@users.noreply.github.com>
2021-07-13 00:01:55 +0300

    updated 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -27,21 +27,3 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
-
-// int main(int argc, char* argv[])
-// {
-// 	int mult;
-
-// 	if (argc != 3)
-// 	{
-// 		printf("Error\n");
-// 		return (1);
-// 	}
-// 	else 
-// 	{
-// 		mult = argv[1] * argv[2];
-
-// 		printf("%d\n", mult);
-// 		return (0);
-// 	}
-// }
