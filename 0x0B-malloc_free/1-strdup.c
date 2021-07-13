b2ac038ed2cb5c5a769f5c5d147b8dd5ccd78a12
Davis Okioma <kuntaytc@gmail.com>
2021-07-13 16:29:29 +0300

    1-strdup.c_ duplicates str to new aloocated memory location

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	{
-		ptr = (char *)malloc(sizeof(char) * strlen(str));
+		ptr = (char *)malloc((*sizeof(char) * strlen(str)) + 1);
 	}
 	if (ptr == NULL)
 		return (NULL);
