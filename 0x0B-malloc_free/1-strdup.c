42bcda1ae292b14cecc3da81b88daeb886033282
Davis Okioma <kuntaytc@gmail.com>
2021-07-13 16:07:07 +0300

    1-strdup.c_ duplicates str to new aloocated memory location

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strdup - returns a ptr to a new memory location, which has a copy of str
+ *
+ * @str: input string to be copied
+ *
+ * Return: A pointer to the array
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	{
+		ptr = (char *)malloc(sizeof(char) * strlen(str));
+	}
+	if (ptr == NULL)
+		return (NULL);
+	{
+		unsigned int i;
+
+		for (i = 0; i < strlen(str); i++)
+		{
+			ptr[i] = str[i];
+		}
+		return (ptr);
+	}
+}
