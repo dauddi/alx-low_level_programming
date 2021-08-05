a2789622503c19dc02ce287c4c6b633a02817543
Davis Okioma <kuntaytc@gmail.com>
2021-08-05 17:46:22 +0300

    0-converts binary number to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,18 +16,21 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num;
 
 	num = 0;
-	if (!b)
-		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+	if (b)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
+		
+		for (i = 0; b[i] != '\0'; i++)
+		{
+			if (b[i] != '0' && b[i] != '1')
+				return (0);
+		}
+		for (i = 0; b[i] != '\0'; i++)
+		{
+			num <<= 1;
+			if (b[i] == '1')
+				num += 1;
+		}
+		return (num);
 	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		num <<= 1;
-		if (b[i] == '1')
-			num += 1;
-	}
-	return (num);
+	return (0);
 }
