6b0f30189ab6a8de0508c94870f090c5650c329f
Davis Okioma <kuntaytc@gmail.com>
2021-08-09 14:41:57 +0300

    allocates memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _calloc - allocates the requested memory and returns a pointer to it.
+ *
+ * @nmemb: a count
+ * @size: size of the memory to print
+ *
+ * Return: success
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *i, ii;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	i = malloc(nmemb * size);
+	if (i == NULL)
+	{
+		return (NULL);
+	}
+	for (ii = 0; ii < nmemb * size; ii++)
+		((char *)i)[ii] = 0;
+		return (i);
+}
