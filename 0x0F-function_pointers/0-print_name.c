cbd535295510f3ad408da2928e37bd846ca1b17a
Davis Okioma <kuntaytc@gmail.com>
2021-11-29 14:40:22 +0300

    initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * print_name - Write a function that prints a name
+ * @name: name of the person
+ * @f: paramter for the function pointer
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	int i;
+
+	char a[10];
+
+	clrscr();
+	if (name == NULL)
+		return (NULL);
+
+	for (i = 0; i <= a; i++);
+	{
+		name[i];
+		getch();
+	}
+}
